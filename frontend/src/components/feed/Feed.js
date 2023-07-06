6169476d46dc217d2beb23579849650fa8b16186
jannisjr <joe_hannis@hotmail.com>
2023-07-06 16:27:18 +0100

    working comments feed

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,26 +1,53 @@
 import React, { useEffect, useState } from "react";
 import Post from "../post/Post";
+import Comment from "../comment/Comment";
 import PostForm from "../post/PostForm";
+import CommentForm from "../comment/CommentForm";
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [comments, setComments] = useState([]);
+  console.log(comments);
 
   useEffect(() => {
-    if (token) {
-      fetch("/posts", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then(async (data) => {
-          console.log(data);
-          window.localStorage.setItem("token", data.token);
-          setToken(window.localStorage.getItem("token"));
-          setPosts(data.posts);
+    const fetchPosts = async () => {
+      if (token) {
+        const response = await fetch("/posts", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         });
-    }
+        const data = await response.json();
+        window.localStorage.setItem("token", data.token);
+        setToken(window.localStorage.getItem("token"));
+        setPosts(data.posts);
+      } else {
+        navigate("/");
+      }
+    };
+
+    fetchPosts();
+  }, [token, navigate]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      if (token) {
+        const response = await fetch("/comments", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        window.localStorage.setItem("token", data.token);
+        setToken(window.localStorage.getItem("token"));
+        setComments(data.comments);
+      } else {
+        navigate("/");
+      }
+    };
+
+    fetchComments();
   }, [token, navigate]);
 
   const logout = () => {
@@ -35,6 +62,14 @@ const Feed = ({ navigate }) => {
       return reversedPosts;
     });
   };
+  const handleNewComment = (comment) => {
+    // console.log(`This is in handleNewComment ${comment}`);
+    setComments((prevComments) => {
+      const newComments = [...prevComments, comment];
+      const reversedComments = newComments.reverse();
+      return reversedComments;
+    });
+  };
 
   if (token) {
     return (
@@ -45,7 +80,19 @@ const Feed = ({ navigate }) => {
           <button onClick={logout}>Logout</button>
           <div id="feed" role="feed">
             {posts.map((post) => (
-              <Post post={post} key={post._id} />
+              <>
+                <Post post={post} key={post._id} />
+                <CommentForm token={token} onNewComment={handleNewComment} />
+                <div>
+                  <div id="feed" role="feed">
+                    {comments.map((comment) => (
+                      <>
+                        <Comment comment={comment} key={comment._id} />
+                      </>
+                    ))}
+                  </div>
+                </div>
+              </>
             ))}
           </div>
         </div>
