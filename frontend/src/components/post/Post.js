430ec052983fd6d175188d91af508d8ef2250e9f
Zara <zarabucknor@yahoo.co.uk>
2023-07-06 16:28:05 +0100

    like and dislike

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Post = ({ post }) => {
-  console.log(post);
+  const [likes, setLikes] = useState(post.likes || 0); // Initialize likes with the existing value or 0
+
+  const handleLike = () => {
+    // Update the likes count
+    setLikes(likes + 1);
+  };
+
   return (
     <div className="post-container" data-cy="post" key={post._id}>
       <div className="username">{post.username}</div>
       <div className="time">{post.time}</div>
       <div className="message">{post.message}</div>
-      <div className="comments">{post.comments}</div>
+      <div className="likes">Likes: {likes}</div> {/* Display the likes count */}
+      <button onClick={handleLike}>
+  {/* Use an emoji, such as a thumbs-up */}
+  <span role="img" aria-label="like">
+    👍
+  </span>
+</button>
+<button onClick={handleLike}>
+  {/* Use an emoji, such as a thumbs-up */}
+  <span role="img" aria-label="dis-like">
+    👎
+  </span>
+</button>
+ {/* Add a like button */}
       <form className="post-form">
         <input type="submit" value="Comment"></input>
         <input type="text" id="name" name="name" required></input>
       </form>
-      {/* <input type="text"></input> */}
     </div>
   );
 };
 
 export default Post;
+
