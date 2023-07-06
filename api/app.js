f041613d608de8c8bd65b8ec9cb7152b4bcb8a47
jannisjr <joe_hannis@hotmail.com>
2023-07-06 11:09:21 +0100

    created route and schema for post comments

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const logger = require("morgan");
 const JWT = require("jsonwebtoken");
 
 const postsRouter = require("./routes/posts");
+const commentsRouter = require("./routes/comments");
 const tokensRouter = require("./routes/tokens");
 const usersRouter = require("./routes/users");
 
@@ -39,6 +40,7 @@ const tokenChecker = (req, res, next) => {
 
 // route setup
 app.use("/posts", tokenChecker, postsRouter);
+app.use("/comments", tokenChecker, commentsRouter);
 app.use("/tokens", tokensRouter);
 app.use("/users", usersRouter);
 app.use("/", postsRouter); // work this out!!
