922a63968fab2f3e171ca9e1834eee3af9162e09
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-07-09 15:50:26 +0100

    add image upload and display functionality

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -42,7 +42,7 @@ const App = () => {
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
-    if (token !== "undefined") {
+    if (token && token !== "null" && token !== "undefined") {
       setIsUserLoggedIn(true);
     }
   }, []);
